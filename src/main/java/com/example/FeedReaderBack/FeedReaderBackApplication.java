42639143244c5537052c12dda500428b88592a86
panya <kjy2844@kaist.ac.kr>
2020-08-05 17:37:26 +0900

    property git ignore

---
+++ src/main/java/com/example/FeedReaderBack/FeedReaderBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.FeedReaderBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeedReaderBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeedReaderBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/FeedReaderBack/FeedReaderBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FeedReaderBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeedReaderBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
